a1170ab9ae42bc6b772954907cd11d2e78117e47
Thierry Chappuis <thierry@chappuis.io>
2019-04-12 10:09:07 +0200

    End of Repository and model implementations

--- diol/core.py
+++ diol/core.py
@@ -1,3 +1,154 @@
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+"""Core module of the dababase IO layer package"""
+
+import re
+import threading
+from dataclasses import dataclass, asdict
+
+import records
+
+_db_connections = {}
+
+class Repository:
+    """Generic repository class suitable for basic database handling."""
+
+    connection_lock = threading.Lock()
+
+    def __init__(self, model):
+        """Initializes the repository.
+
+        Args:
+            model (type): dataclass representing the database entity.
+
+        """
+        # Initialisation of the connection to the database
+        self._connection_key = None
+        with self.connection_lock:
+            if not self._connection_key:
+                self._connection_key = 'default'
+                _db_connections[self._connection_key] = records.Database()
+            self._db = _db_connections[self._connection_key]
+        # Entity-related attributes
+        self.model = model
+        self.model_name = model.__name__
+        if hasattr(model, 'table_name'):
+            self.table_name = model.table_name
+        else:
+            parts = re.findall('[A-Z][^A-Z]*', self.model_name)
+            self.table_name = "_".join(parts).lower().strip()
+
+
+    @property
+    def _last_id(self):
+        """Returns the last auto-generated ID."""
+        rows = self._db.query("""
+            SELECT LAST_INSERT_ID() as id
+        """).all(as_dict=True)
+        if len(rows) and 'id' in rows[0]:
+            return rows[0]['id']
+        return None
+
+    def _columns(self, data):
+        """Generates SQL line column selection in select or insert queries."""
+        return ", ".join([col for col in data])
+
+    def _placeholders(self, data):
+        """Generates SQL line for placeholders for values in insert queries."""
+        return ", ".join([f":{col}" for col in data])
+
+    def _where(self, data):
+        """Generates SQL line for where conditions in select queries."""
+        return " AND ".join([f"{col}=:{col}" for col in data])
+
+    def _create(self, data):
+        """Creates a new database entry using the given dictionnary."""
+        self._db.query(f"""
+            INSERT INTO {self.table_name}({self._columns(data)})
+            VALUES ({self._placeholders(data)})
+        """, **data)
+
+    def _get_all(self):
+        """Selects all the entries in the considered table."""
+        return self._db.query(f"""
+            SELECT * FROM {self.table_name}
+        """).all(as_dict=True)
+
+    def _get_last(self):
+        """Selects the last inserted query."""
+        return self._db.query(f"""
+            SELECT * FROM {self.table_name} WHERE id=:id
+        """, id=self._last_id).all(as_dict=True)
+
+    def _get_all_by(self, data):
+        """Selects all the database entries that match the given data."""
+        return self._db.query(f"""
+            SELECT * FROM {self.table_name} WHERE {self._where(data)}
+        """, **data).all(as_dict=True)
+
+    def create(self, **data):
+        """Creates a new entry and returns the corresponding instance."""
+        self._create(data)
+        if 'id' not in data:
+            data['id'] = self.last_id
+        return self.model(**data)
+
+
+    def get_or_create(self, **data):
+        """Selects an entry based on the given data and creates one if nothing
+        is found.
+        """
+        rows = self._get_all_by(data)
+        if not rows:
+            self._create(data)
+            rows = self._get_last()
+
+        return self.model(**rows[0])
+
+    def filter(self, **data):
+        """Selects all database entries that match the given data or an empty
+        list if nothing is found.
+        """
+        rows = self._get_all_by(data)
+        return [self.model(**elem) for elem in rows]
+
+    def get(self, **data):
+        """Selects the first data entry that match the given data or None if
+        nothing is found.
+        """
+        rows = self._get_all_by(data)
+        if rows:
+            return self.model(**rows[0])
+        return None
+
+    def save(self, instance):
+        """Saves a new instance in the database."""
+        data = {k: v for k, v in asdict(instance).items() if v is not None}
+        self._create(data)
+        if not instance.id:
+            instance.id = self._last_id
+
+    def save_all(self, collection):
+        """Saves a collection of new instances in the database."""
+        if collection:
+            for instance in collection:
+                self.save(instance)
+
+    def get_all(self):
+        """Selects all the entries of the corresponding entity in the database.
+        """
+        rows = self._get_all()
+        return [self.model(**elem) for elem in rows]
+
+def model(entity):
+    """Class decorator to create models.
+
+    The decorator transforms the entity into a dataclass and injects a
+    repository instance as class attribute objects as well as a save method.
+
+    """
+    entity = dataclass(entity)
+    if not hasattr(entity, 'objects'):
+        entity.objects = Repository(entity)
+    entity.save = lambda self: entity.objects.save(self)
+    return entity
--- tests/test_core.py
+++ tests/test_core.py
@@ -3,4 +3,273 @@
 
 """Tests for `diol.core` module."""
 
+from dataclasses import dataclass
+
+from diol.core import Repository, model
+
+def test_repository_creates_as_expected(mocker):
+    class MyFakeEntity:
+        pass
+    mocker.patch('records.Database')
+    repository = Repository(MyFakeEntity)
+    assert repository.model == MyFakeEntity
+    assert repository.model_name == 'MyFakeEntity'
+    assert repository.table_name == "my_fake_entity"
+
+def test_repository_recognizes_table_name_option(mocker):
+    class MyFakeEntity:
+        table_name = "my_super_table_name"
+    mocker.patch('records.Database')
+    repository = Repository(MyFakeEntity)
+    assert repository.model_name == 'MyFakeEntity'
+    assert repository.table_name == "my_super_table_name"
+
+def test_repository_last_id_returns_id(mocker):
+    class MyFakeEntity:
+        pass
+    ID = 77
+    mocker.patch('records.Database')().query().all.return_value = [{'id': ID}]
+    repository = Repository(MyFakeEntity)
+    assert repository._last_id == ID
+
+def test_repository_last_id_returns_none(mocker):
+    class MyFakeEntity:
+        pass
+    mocker.patch('records.Database')().query().all.return_value = []
+    repository = Repository(MyFakeEntity)
+    assert repository._last_id is None
+
+def test_repository_create_generate_correct_sql(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    repository = Repository(MyFakeEntity)
+    instance = repository.create(**PARAMS_DICT)
+    args, kwargs = Database().query.call_args
+    assert 'INSERT INTO my_fake_entity(id, title, content)' in args[0]
+    assert 'VALUES (:id, :title, :content)' in args[0]
+
+def test_repository_create_returns_instance(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    repository = Repository(MyFakeEntity)
+    instance = repository.create(**PARAMS_DICT)
+    assert isinstance(instance, MyFakeEntity)
+
+def test_repository_get_or_create_generate_correct_getallby_sql(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = [PARAMS_DICT]
+    repository = Repository(MyFakeEntity)
+    instance = repository.get_or_create(**PARAMS_DICT)
+    args, kwargs = Database().query.call_args
+    assert 'SELECT * FROM my_fake_entity' in args[0]
+    assert 'WHERE id=:id AND title=:title AND content=:content' in args[0]
+
+def test_repository_get_or_create_generate_correct_create_sql(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.side_effect = [
+        [], [PARAMS_DICT], [PARAMS_DICT], [PARAMS_DICT], [PARAMS_DICT]
+    ]
+    repository = Repository(MyFakeEntity)
+    instance = repository.get_or_create(**PARAMS_DICT)
+    args, kwargs = Database().query.call_args_list[2]
+    assert 'INSERT INTO my_fake_entity(id, title, content)' in args[0]
+    assert 'VALUES (:id, :title, :content)' in args[0]
+
+def test_repository_get_or_create_generate_correct_create_sql(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.side_effect = [
+        [], [PARAMS_DICT], [PARAMS_DICT], [PARAMS_DICT], [PARAMS_DICT]
+    ]
+    repository = Repository(MyFakeEntity)
+    instance = repository.get_or_create(**PARAMS_DICT)
+    args, kwargs = Database().query.call_args_list[2]
+    assert 'INSERT INTO my_fake_entity(id, title, content)' in args[0]
+    assert 'VALUES (:id, :title, :content)' in args[0]
+
+def test_repository_get_or_create_generate_correct_getlast_sql(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.side_effect = [
+        [], [PARAMS_DICT], [PARAMS_DICT], [PARAMS_DICT], [PARAMS_DICT]
+    ]
+    repository = Repository(MyFakeEntity)
+    instance = repository.get_or_create(**PARAMS_DICT)
+    args, kwargs = Database().query.call_args_list[4]
+    assert 'SELECT * FROM my_fake_entity WHERE id=:id' in args[0]
+
+def test_repository_get_or_create_generate_does_not_create_if_found(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = [PARAMS_DICT]
+    repository = Repository(MyFakeEntity)
+    instance = repository.get_or_create(**PARAMS_DICT)
+    assert len(Database().query.call_args_list) == 2
+
+def test_repository_get_or_create_returns_instance_if_found(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = [PARAMS_DICT]
+    repository = Repository(MyFakeEntity)
+    instance = repository.get_or_create(**PARAMS_DICT)
+    assert isinstance(instance, MyFakeEntity)
+
+def test_repository_get_or_create_returns_instance_if_created(mocker):
+    PARAMS_DICT = {'id':1, 'title': 'essai', 'content': 'lorem ipsum'}
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.side_effect = [
+        [], [PARAMS_DICT], [PARAMS_DICT], [PARAMS_DICT], [PARAMS_DICT]
+    ]
+    repository = Repository(MyFakeEntity)
+    instance = repository.get_or_create(**PARAMS_DICT)
+    assert isinstance(instance, MyFakeEntity)
+
+def test_repository_filter_returns_instances_if_found(mocker):
+    ROWS = [
+        {'id':1, 'title': 'essai', 'content': 'lorem ipsum'},
+        {'id':2, 'title': 'essai 2', 'content': 'lorem ipsum 2'}
+    ]
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = ROWS
+    repository = Repository(MyFakeEntity)
+    instances = repository.filter(title='essai', content='lorem')
+    assert isinstance(instances[0], MyFakeEntity)
+    assert isinstance(instances[1], MyFakeEntity)
+
+def test_repository_filter_returns_empty_list_if_nothing_found(mocker):
+    ROWS = []
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = ROWS
+    repository = Repository(MyFakeEntity)
+    instances = repository.filter(title='essai', content='lorem')
+    assert instances == []
+
+def test_repository_get_returns_instance_if_found(mocker):
+    ROWS = [
+        {'id':1, 'title': 'essai', 'content': 'lorem ipsum'},
+        {'id':2, 'title': 'essai 2', 'content': 'lorem ipsum 2'}
+    ]
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = ROWS
+    repository = Repository(MyFakeEntity)
+    instance = repository.get(title='essai', content='lorem')
+    assert isinstance(instance, MyFakeEntity)
+
+def test_repository_get_returns_none_if_nothing_found(mocker):
+    ROWS = []
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = ROWS
+    repository = Repository(MyFakeEntity)
+    instance = repository.get(title='essai', content='lorem')
+    assert instance is None
+
+def test_repository_save_generate_correct_create_sql(mocker):
+    @dataclass
+    class MyFakeEntity:
+        title: str
+        content: str
+        id: int = None
+    Database = mocker.patch('records.Database')
+    repository = Repository(MyFakeEntity)
+    repository.save(MyFakeEntity(title='essai', content='lorem'))
+    args, kwargs = Database().query.call_args_list[0]
+    assert 'INSERT INTO my_fake_entity(title, content)' in args[0]
+    assert 'VALUES (:title, :content)' in args[0]
+    assert kwargs['title'] == 'essai'
+    assert kwargs['content'] == 'lorem'
+    assert 'id' not in kwargs
+
+def test_repository_get_all_returns_instances_if_found(mocker):
+    ROWS = [
+        {'id':1, 'title': 'essai', 'content': 'lorem ipsum'},
+        {'id':2, 'title': 'essai 2', 'content': 'lorem ipsum 2'}
+    ]
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = ROWS
+    repository = Repository(MyFakeEntity)
+    instances = repository.get_all()
+    assert isinstance(instances[0], MyFakeEntity)
+    assert isinstance(instances[1], MyFakeEntity)
+
+def test_repository_get_all_returns_empty_list_if_nothing_found(mocker):
+    ROWS = []
+    class MyFakeEntity:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+    Database = mocker.patch('records.Database')
+    Database().query().all.return_value = ROWS
+    repository = Repository(MyFakeEntity)
+    instances = repository.get_all()
+    assert instances == []
+
+def test_model_decorator_transforms_to_dataclass(mocker):
+    mocker.patch('records.Database')
+    @model
+    class MyFakeEntity:
+        title: str
+        content: str
+        id: int = None
+    instance = MyFakeEntity(title='title', content='content', id=1)
+    assert instance.id == 1
+    assert instance.title == 'title'
+    assert instance.content == 'content'
+    assert isinstance(MyFakeEntity.objects, Repository)
+    assert MyFakeEntity.objects.model == MyFakeEntity
+
+def test_model_decorator_save_calls_repository_save(mocker):
+    mocker.patch('records.Database')
+    mocker.patch('diol.core.Repository')
+    @model
+    class MyFakeEntity:
+        title: str
+        content: str
+        id: int = None
+    instance = MyFakeEntity(title='title', content='content', id=1)
+    instance.save()
+    args, kwargs = instance.objects.save.call_args
+    assert args[0] == instance
 
